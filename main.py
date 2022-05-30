ae3dacffa31e3d8b627fa09a7ded827593a86a6a
andrey-baykov <baykov.andrey@gmail.com>
2022-05-30 02:14:48 -0700

    Add create a new company

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ data = {
   "password": "12345"
 }
 token = {"Authorization": ""}
-new_candidate_token = {"Authorization": ""}
 
 response = requests.post(BASE_URL + "/login", json=data)
 if response.status_code != 200:
@@ -31,13 +30,13 @@ candidate = {
   "summary": "Account for the perfect person"
 }
 
-response = requests.post(BASE_URL + "/candidates", json=candidate, headers=token)
-if response.status_code != 201:
-    print(f"{response.status_code}: Can't create a new candidate. Maybe it is exists")
+response2 = requests.post(BASE_URL + "/candidates", json=candidate, headers=token)
+if response2.status_code != 201:
+    print(f"{response2.status_code}: Can't create a new candidate. Maybe it is exists")
 else:
-    lines = response.json()
-    candidate_id = lines["id"]
-    print(lines)
+    lines2 = response2.json()
+    candidate_id = lines2["id"]
+    print(lines2)
     print(f"New id: {candidate_id}")
 
 # Login as new candidate
@@ -46,15 +45,34 @@ new_candidate_data = {
   "password": "12345"
 }
 
-new_candidate_token = {"Authorization": ""}
-response = requests.post(BASE_URL + "/login", json=new_candidate_data)
-if response.status_code != 200:
-    print(f"{response.status_code}: Can't reach token")
+response3 = requests.post(BASE_URL + "/login", json=new_candidate_data)
+if response3.status_code != 200:
+    print(f"{response3.status_code}: Can't reach token")
     exit()
 else:
-    new_candidate_token["Authorization"] = "Bearer " + response.json()['token']
-print(token)
-print(new_candidate_token)
+    token["Authorization"] = "Bearer " + response3.json()['token']
+
+# Create a new position
+position_data = {
+    "title": "Python QA automation engineer",
+    "address": "1st Main campus",
+    "city": "San Jose",
+    "state": "CA",
+    "zip": "93145",
+    "description": "SQA Automation engineer on python. Needs to automate all in the our company",
+    "dateOpen": "2022-05-30",
+    "company": "Space technologies"
+}
+
+response4 = requests.post(BASE_URL + "/positions", json=position_data,  headers=token)
+if response4.status_code != 201:
+    print(f"{response4.status_code}: Can't create a new position. Maybe it is exists")
+else:
+    lines4 = response4.json()
+    company_id = lines4["id"]
+    print(lines4)
+    print(f"New id: {company_id}")
+
 
 exit()
 # Reach list of all candidates
